13715aeee1cb0e631647166e2ad20de3a24e964d
Yesset <poxerface5@gmail.com>
2024-09-12 13:19:41 +0500

    feat: department title

--- resources/js/Components/Reusable/WallPosting/UserPostTitle.jsx
+++ resources/js/Components/Reusable/WallPosting/UserPostTitle.jsx
@@ -8,9 +8,24 @@ export function CommunityTitle({ post }) {
     );
 }
 
+export function DepartmentTitle({ post }) {
+    // TODO: check roles, if not admin or superadmin, show community name without poster name
+    const isAdmin = true;
+    const title =
+        post.department.name + (isAdmin ? ` (${post.user.name})` : "");
+
+    return (
+        <div className="text-base font-semibold text-neutral-800">{title}</div>
+    );
+}
+
 export function UserPostTitle({ post }) {
     if (post.community) {
         return <CommunityTitle post={post} />;
+    }
+
+    if (post.department) {
+        return <DepartmentTitle post={post} />;
     }
 
     return (
