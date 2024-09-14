88465b17fed84b3b65bf5d2a57429a397ed7ad4e
Yesset <poxerface5@gmail.com>
2024-09-14 18:53:10 +0500

    fix: limits

--- setup_limits.sh
+++ setup_limits.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Set buffer limits in FastCGI
-echo "fastcgi_buffers 16 16k;" >> /etc/nginx/nginx.conf
-echo "fastcgi_buffer_size 32k;" >> /etc/nginx/nginx.conf
+PHP_INI_PATH=$(php -i | grep '/php.ini' | awk '{print $6}')
 
 # Set file upload limits in php.ini
-echo "upload_max_filesize = 20M" >> /etc/php/php.ini
-echo "post_max_size = 20M" >> /etc/php/php.ini
+echo "\nupload_max_filesize = 20M" >> $PHP_INI_PATH
+echo "\npost_max_size = 20M\n" >> $PHP_INI_PATH
