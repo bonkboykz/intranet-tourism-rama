9a117a4b2bab9d871ec01eb6b010a7341302f467
Yesset <poxerface5@gmail.com>
2024-09-17 12:58:51 +0500

    fix: double login button

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -23,11 +23,7 @@ export default function Login({ status, canResetPassword }) {
 
     const submit = (e) => {
         e.preventDefault();
-        post("/login", {
-            onSuccess: () => {
-                window.location.href = "/dashboard";
-            },
-        });
+        post("/login");
     };
 
     useEffect(() => {
--- resources/js/Plugins/http.js
+++ resources/js/Plugins/http.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
 axios.interceptors.response.use(
     (response) => response,
     async (err) => {
