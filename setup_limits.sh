0366a8a758b779f51ecaaf3551bf1bc9106122c4
Yesset <poxerface5@gmail.com>
2024-09-17 11:50:31 +0500

    feat: update limits

--- setup_limits.sh
+++ setup_limits.sh
@@ -5,7 +5,7 @@ PHP_INI_PATH=$(php -i | grep '/php.ini' | awk '{print $6}')
 echo "PHP_INI_PATH: $PHP_INI_PATH"
 
 # Set file upload limits in php.ini
-echo "\nupload_max_filesize = 20M" >> $PHP_INI_PATH
-echo "\npost_max_size = 20M" >> $PHP_INI_PATH
+echo "\nupload_max_filesize = 120M" >> $PHP_INI_PATH
+echo "\npost_max_size = 120M" >> $PHP_INI_PATH
 
 # echo "\nzend.exception_ignore_args = Off" >> $PHP_INI_PATH
