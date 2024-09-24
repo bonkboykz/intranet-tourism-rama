dbb85953dc77810ba4a7caf7a3ade007a8cc9da0
Yesset <poxerface5@gmail.com>
2024-09-24 12:51:05 +0500

    fix: download icon

--- resources/js/Components/Reusable/WallPosting/PostAttachments.jsx
+++ resources/js/Components/Reusable/WallPosting/PostAttachments.jsx
@@ -55,44 +55,60 @@ function PostAttachments({ attachments }) {
 
     const renderDocument = (attachment, index) => {
         const handleDownload = (e, attachment) => {
-            e.preventDefault();  
+            e.preventDefault();
             const fileUrl = `/storage/${attachment.path}`;
-    // For SVG icons, open the file in a new tab
-    if (attachment.extension === 'svg') {
-        window.open(fileUrl, '_blank');
-    } else {
-            const link = document.createElement('a');
-            link.href = `/storage/${attachment.path}`;
-            link.download = attachment.metadata.original_name;
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
+            // For SVG icons, open the file in a new tab
+            if (attachment.extension === "svg") {
+                window.open(fileUrl, "_blank");
+            } else {
+                const link = document.createElement("a");
+                link.href = `/storage/${attachment.path}`;
+                link.download = attachment.metadata.original_name;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
             }
         };
 
-    const handleView = (e, attachment) => {
-        e.stopPropagation(); 
-        const fileUrl = `/storage/${attachment.path}`;
-        
-        if (attachment.extension === 'pdf') {
-            window.open(fileUrl, '_blank');
-        } else {
-            handleDownload(e, attachment); 
-        }   
-    };
+        const handleView = (e, attachment) => {
+            e.stopPropagation();
+            const fileUrl = `/storage/${attachment.path}`;
+
+            if (attachment.extension === "pdf") {
+                window.open(fileUrl, "_blank");
+            } else {
+                handleDownload(e, attachment);
+            }
+        };
 
         return (
-            <article key={index} className="flex gap-3 items-center py-1.5 px-4 mb-2 bg-gray-100 rounded-xl border-2 border-gray-200 max-w-[900px]" onClick={(e) => handleView(e, attachment)} style={{ cursor: 'pointer' }}>
+            <article
+                key={index}
+                className="flex gap-3 items-center py-1.5 px-4 mb-2 bg-gray-100 rounded-xl border-2 border-gray-200 max-w-[900px]"
+                onClick={(e) => handleView(e, attachment)}
+                style={{ cursor: "pointer" }}
+            >
                 <img
                     src={
-                        attachment.extension === 'pdf' ? PDF :
-                        attachment.extension === 'docx' || attachment.extension === 'doc' ? DOC :
-                        attachment.extension === 'xlsx' ? Excel :
-                        attachment.extension === 'pptx' || attachment.extension === 'ptx' ? PowerPoint :
-                        attachment.extension === 'txt' ? TXT :
-                        'path/to/default-icon.png'
+                        attachment.extension === "pdf"
+                            ? PDF
+                            : attachment.extension === "docx" ||
+                                attachment.extension === "doc"
+                              ? DOC
+                              : attachment.extension === "xlsx"
+                                ? Excel
+                                : attachment.extension === "pptx" ||
+                                    attachment.extension === "ptx"
+                                  ? PowerPoint
+                                  : attachment.extension === "txt"
+                                    ? TXT
+                                    : "path/to/default-icon.png"
                     }
-                    style={{ width: '18px', height: '18px', objectFit: 'contain' }}
+                    style={{
+                        width: "18px",
+                        height: "18px",
+                        objectFit: "contain",
+                    }}
                     alt={`${attachment.extension} file`}
                 />
                 <div className="flex flex-col items-start flex-grow">
@@ -100,9 +116,15 @@ function PostAttachments({ attachments }) {
                         Download File
                     </span>
                 </div>
-                <img src="/assets/downloadIcon3.svg" alt="Download Icon" className="w-3.5 h-3.5 opacity-70 cursor-auto" 
-                onClick={(e) =>{e.stopPropagation();  handleDownload(e, attachment);
-    }}/>
+                <img
+                    src="/assets/downloadIconNo3.svg"
+                    alt="Download Icon"
+                    className="w-3.5 h-3.5 opacity-70 cursor-auto"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        handleDownload(e, attachment);
+                    }}
+                />
             </article>
         );
     };
