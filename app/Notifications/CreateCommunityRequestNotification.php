7e7867405d36aa2e8b864e3210e9cab5ce2e3e72
Yesset <poxerface5@gmail.com>
2024-10-07 17:38:46 +0500

    feat: create community requests

--- app/Notifications/CreateCommunityRequestNotification.php
+++ app/Notifications/CreateCommunityRequestNotification.php
@@ -1,22 +1,21 @@
 <?php
+
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
 
-// This notification informs an admin or superuser when a user requests to join a group.
-class GroupJoinRequestNotification extends Notification implements ShouldQueue
+class CreateCommunityRequestNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    public $group;
+    public $community;
     public $user;
 
     public $request;
-
 
     public function __construct($request)
     {
@@ -32,14 +31,14 @@ class GroupJoinRequestNotification extends Notification implements ShouldQueue
     {
         if ($this->request->status !== 'pending') {
             return [
-                'message' => 'Your request to join a group has been ' . $this->request->status . '.',
+                'message' => 'Your request to create a community has been ' . $this->request->status . '.',
                 'request_id' => $this->request->id, // Store the request ID in the notification
                 'details' => $this->request->details,
             ];
         }
 
         return [
-            'message' => 'New request to join a group.',
+            'message' => 'New request to create a community.',
             'request_id' => $this->request->id, // Store the request ID in the notification
             'details' => $this->request->details,
         ];
@@ -48,28 +47,27 @@ class GroupJoinRequestNotification extends Notification implements ShouldQueue
     // Broadcast notification format (for Laravel Echo + Reverb)
     public function toBroadcast($notifiable)
     {
-        // if request was updated (accepted, reject) send a notification to the user
+        // if request was updated (accepted, rejected) send a notification to the user
         if ($this->request->status !== 'pending') {
             return new BroadcastMessage([
-                'message' => 'Your request to join a group has been ' . $this->request->status . '.',
+                'message' => 'Your request to create a community has been ' . $this->request->status . '.',
                 'request_id' => $this->request->id,
                 'details' => $this->request->details,
             ]);
         }
 
         return new BroadcastMessage([
-            'message' => 'New request to join a group.',
+            'message' => 'New request to create a community.',
             'request_id' => $this->request->id,
             'details' => $this->request->details,
         ]);
     }
 
-
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->line($this->user->name . ' has requested to join the group ' . $this->group->name)
-            ->action('Review Request', url('/groups/' . $this->group->id . '/requests'))
+            ->line($this->user->name . ' has requested to create the community ' . $this->community->name)
+            ->action('Review Request', url('/communities/' . $this->community->id . '/requests'))
             ->line('Please review the request and take appropriate action.');
     }
 }
