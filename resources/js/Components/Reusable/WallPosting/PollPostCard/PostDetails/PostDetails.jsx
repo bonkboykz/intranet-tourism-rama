806f6780f63acdd98f565c397020343b7b1e4b50
Yesset <poxerface5@gmail.com>
2024-10-07 18:06:21 +0500

    fix: return announcements on polls

--- resources/js/Components/Reusable/WallPosting/PollPostCard/PostDetails/PostDetails.jsx
+++ resources/js/Components/Reusable/WallPosting/PollPostCard/PostDetails/PostDetails.jsx
@@ -11,7 +11,18 @@ export function PostDetails({
     onClose,
     onExport,
     popupRef,
+    onAnnouncement,
+    isAnnounced,
 }) {
+    const { hasRole } = usePermissions();
+
+    const isSuperAdmin = hasRole("superadmin");
+    const { isAdmin: isCommunityAdmin } = useContext(CommunityContext);
+    const { isAdmin: isDepartmentAdmin } = useContext(DepartmentContext);
+
+    const canMakeAnnouncement =
+        isSuperAdmin || isCommunityAdmin || isDepartmentAdmin;
+
     return (
         <div
             className="absolute bg-white border-2 rounded-xl p-1 shadow-custom mt-16 right-0 w-[180px] h-auto z-10"
@@ -69,6 +80,22 @@ export function PostDetails({
                 />
                 Delete poll
             </p>
+            {canMakeAnnouncement && (
+                <>
+                    <div className="font-extrabold text-neutral-800 my-1 border-b-2 border-neutral-200"></div>
+                    <p
+                        className="cursor-pointer flex flex-row hover:bg-blue-100 rounded-xl p-2"
+                        onClick={onAnnouncement}
+                    >
+                        <img
+                            className="w-6 h-6 mr-2"
+                            src="/assets/AnnounceIcon.svg"
+                            alt="Announcement"
+                        />
+                        {isAnnounced ? "Unannounce" : "Announce"}
+                    </p>
+                </>
+            )}
         </div>
     );
 }
