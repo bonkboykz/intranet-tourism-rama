9bbc73611eedc6f5b0ebeb2df058800af782e26e
Yesset <poxerface5@gmail.com>
2024-10-09 01:07:05 +0500

    fix: announcement header position

--- resources/js/Components/Reusable/WallPosting/DefaultPostCard/DefaultPostCard.jsx
+++ resources/js/Components/Reusable/WallPosting/DefaultPostCard/DefaultPostCard.jsx
@@ -474,7 +474,14 @@ export function DefaultPostCard({ post }) {
                 )}
             >
                 {cachedPost.announced && (
-                    <div className="absolute w-full top-0 left-0  bg-secondary h-14 rounded-t-2xl pl-6">
+                    <div
+                        className="absolute w-full top-0 left-0  bg-secondary h-14 rounded-t-2xl pl-6"
+                        style={{
+                            top: -2,
+                            width: "calc(100% + 2px)",
+                            left: -1,
+                        }}
+                    >
                         <div className="flex items-center gap-1 w-full h-full">
                             <Volume2 className="w-6 h-6 text-white" />
                             <div className="text-white text-center font-bold text-lg	ml-2">
