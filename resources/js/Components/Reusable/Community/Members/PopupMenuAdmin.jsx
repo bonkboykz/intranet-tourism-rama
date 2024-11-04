99a444d7c4eaa2c24901270786274cd94fdf61df
bonkboykz <poxerface5@gmail.com>
2024-11-04 05:25:15 +0500

    fix: department && community demoting/assigning admin

--- resources/js/Components/Reusable/Community/Members/PopupMenuAdmin.jsx
+++ resources/js/Components/Reusable/Community/Members/PopupMenuAdmin.jsx
@@ -1,28 +1,9 @@
-import { toast } from "react-toastify";
-
 export const PopupMenuAdmin = ({ onAssign, onRemove, isOnlyAdmin }) => {
     return (
         <div className="relative">
             <div className="absolute right-0 z-50 bg-white border shadow-lg w-[190px] rounded-xl -mt-3">
                 <button
-                    onClick={() => {
-                        if (isOnlyAdmin) {
-                            toast.error(
-                                "A community group needs at least 1 admin.",
-                                {
-                                    position: "top-right",
-                                    autoClose: 5000,
-                                    hideProgressBar: false,
-                                    closeOnClick: true,
-                                    pauseOnHover: true,
-                                    draggable: true,
-                                    progress: undefined,
-                                }
-                            );
-                            return;
-                        }
-                        onAssign();
-                    }}
+                    onClick={onAssign}
                     className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:rounded-t-xl"
                 >
                     <img
@@ -33,24 +14,7 @@ export const PopupMenuAdmin = ({ onAssign, onRemove, isOnlyAdmin }) => {
                     Demote to Member
                 </button>
                 <button
-                    onClick={() => {
-                        if (isOnlyAdmin) {
-                            toast.error(
-                                "A community group needs at least 1 admin.",
-                                {
-                                    position: "top-right",
-                                    autoClose: 5000,
-                                    hideProgressBar: false,
-                                    closeOnClick: true,
-                                    pauseOnHover: true,
-                                    draggable: true,
-                                    progress: undefined,
-                                }
-                            );
-                            return;
-                        }
-                        onRemove();
-                    }}
+                    onClick={onRemove}
                     className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:rounded-b-xl"
                 >
                     <img
